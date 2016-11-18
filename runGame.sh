02801f337ad79febfe5c8d8f87340596b8d95782
Zeming Lin <ebetica0@gmail.com>
2016-11-18 03:10:52 -0500

    Run game to take care of the python part for you

--- runGame.sh
+++ runGame.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-./halite -d "50 50" "python3 $1" "python3 $2" "python3 $3" "python3 $4" -q
+if [ "$#" -eq 3 ]; then
+  ./halite -d "$1" "python3 $2" "python3 $3" -q
+elif [ "$#" -eq 4 ]; then
+  ./halite -d "$1" "python3 $2" "python3 $3" "python3 $4" -q
+elif [ "$#" -eq 5 ]; then
+  ./halite -d "$1" "python3 $2" "python3 $3" "python3 $4" "python3 $5" -q
+elif [ "$#" -eq 6 ]; then
+  ./halite -d "$1" "python3 $2" "python3 $3" "python3 $4" "python3 $5" "python3 $6" -q
+fi
